6cabb4becf4e5cf5a77877930ac36c83a82efe3d
Wielfried Zouantcha <zouantchaw74@gmail.com>
2020-04-09 03:54:56 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,6 +1,6 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
-require_relative './fancy_'
+require_relative './fancy_dance.rb'
 
 class Dancer 
   extend FancyDance::ClassMethods
