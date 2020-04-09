26948fbbe374e577714fbc4d868d609d957e9a33
Wielfried Zouantcha <zouantchaw74@gmail.com>
2020-04-09 03:43:36 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -2,4 +2,11 @@ require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 
 class Dancer 
+  include Dance
+ 
+  attr_accessor :name
+ 
+  def initialize(name)
+    @name = name
+  end
 end 
